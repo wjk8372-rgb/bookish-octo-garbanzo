24b21d90d8dc9f8c0e61cbc8d57978966d83c5d0
wjk8372-rgb <wjk8372@gmail.com>
2026-08-26 17:27:02 +0800

    Delete api/health.js

--- api/health.js
+++
@@ -1,8 +0,0 @@
-const kuaidi = require('../kuaidi');
-
-module.exports = async function handler(req, res) {
-  return res.status(200).json({
-    ok: true, time: new Date().toISOString(),
-    kuaidiMockMode: kuaidi.isMockMode(),
-  });
-};
